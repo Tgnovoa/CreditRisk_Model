daf26b4df69685cd27b5f6530150377194311ae3
Santiago <gnovoa@tresalia.com>
2018-11-02 12:01:43 -0600

    Creacion de EWMA para cada observacion de la base

--- 1_scripts/4_MetricaProbabilidadDeterioro.R
+++ 1_scripts/4_MetricaProbabilidadDeterioro.R
@@ -63,7 +63,7 @@ read_ratings <- read_ratings %>%
                      dplyr::select(Rating,pd))
 # Model T (= times to "jump" outside grade) as an exponential process ----
 exp_mean_cens <- read_csv("csv/media_cens.csv")
-
+# the parameters for the distributions were extracted from the benchmark transition matrix data and using an assumption of censored observations
 # Model the conditional probability of change P(new_rating|change_of_rating) as a multinomial variable (with the benchmarks historical data) ----
 ratings_arrange <- read_ratings %>% 
   dplyr::filter(!is.na(Date)) %>% 
@@ -104,8 +104,64 @@ for(i in 1:(nrow(multi_tm_prob)-1)){
   }
 }
 join_multinom_matrix[21,] <- (multi_tm_prob[21,] + Pmat[21,])/2
+# the decision to give the same weight to moodys_tm data and historical benchmark data was made to make easier the process, 
+# in the future it would be useful to analyse (with a bayesian framework) this assumptions
 #### EWMA ----
-# construct variables quarters_to_change, quarters_since_change, next_rating, previous_rating
+# construct variables  next_rating, previous_rating and add diff from pd to create ewma ----
+start_date_ratings <- read_ratings %>% 
+  dplyr::arrange(CompanyName, ISIN, CIQ_ID, Date) %>% 
+  dplyr::group_by(CompanyName) %>% 
+  dplyr::filter(!is.na(Date)) %>% 
+  dplyr::filter(!is.na(Rating)) %>% 
+  dplyr::summarise(start_rating = min(DateQ, na.rm = T))
+db_ratings_arr <- read_ratings %>% 
+  dplyr::left_join(start_date_ratings) %>% 
+  dplyr::filter(DateQ>=start_rating) %>% 
+  dplyr::arrange(CompanyName, ISIN, CIQ_ID, Date)
+db_ratings_NA <- db_ratings_arr %>% 
+  dplyr::mutate(Rating_NA = factor(ifelse(is.na(Rating), "NA_Rating", Rating %>% as.character()),
+                                   levels = c("AAA",
+                                              "AA+","AA","AA-",
+                                              "A+","A","A-",
+                                              "BBB+","BBB","BBB-",
+                                              "BB+","BB","BB-",
+                                              "B+","B","B-",
+                                              "CCC+","CCC","CCC-",
+                                              "CC","C", "NA_Rating")))
+pre_rating_aux <- rep("NA_Rating", nrow(db_ratings_NA))
+pre_pd_aux <- rep(NA_real_, nrow(db_ratings_NA))
+for(i in 2:nrow(db_ratings_NA)){
+  if( db_ratings_NA$DateQ[i] > db_ratings_NA$start_rating[i] ){
+    if( db_ratings_NA$Rating_NA[i] == db_ratings_NA$Rating_NA[i-1] ){
+      pre_rating_aux[i] <- pre_rating_aux[i-1]
+      pre_pd_aux[i] <- pre_pd_aux[i-1]
+    }else{
+      pre_rating_aux[i] <- db_ratings_NA$Rating_NA[i-1] %>% as.character()
+      pre_pd_aux[i] <- db_ratings_NA$pd[i-1]
+    } 
+  }
+}
+db_ratings_NA$prev_rating <- pre_rating_aux
+db_ratings_NA$pd_aux <- pre_pd_aux
+db_ratings_NA <- db_ratings_NA %>% 
+  dplyr::mutate(pd_ewma_aux = ifelse(is.na(Rating),pd_aux, pd))
+db_ratings_NA <- db_ratings_NA %>% 
+  dplyr::group_by(CIQ_ID) %>% 
+  dplyr::mutate(pd_diff = ifelse(DateQ == start_rating, 0, pmax(pd_ewma_aux - lag(pd_ewma_aux),0)))
+
+# calculate ewma of pd_diff with exponentialy weighting of pd_diff ----
+lambda_ewma <- 0.4
+# lambda_ewma is one of the parameters that should be analysed in the future to optimize the results 
+db_ratings_NA$ewma <- rep(0.0,nrow(db_ratings_NA))
+for(i in nrow(db_ratings_NA):1){
+  sum_ewma <- 0
+  dist_date_ewma <- as.integer(round(4*(db_ratings_NA$DateQ[i]-db_ratings_NA$start_rating[i]),0))+1
+  for(j in (dist_date_ewma:1)){
+    sum_ewma = sum_ewma + db_ratings_NA$pd_diff[i-j+1]*lambda_ewma^(j-1)*(1-lambda_ewma)
+  }
+  db_ratings_NA$ewma[i] <- sum_ewma
+}
+
 #### Bonds valuation ----
 # load read_bonds and add perc to matrix to get "new starting rating"
 load("3_MetricaSpreadBonos/3_MetricaSpreadBonos.RData")
